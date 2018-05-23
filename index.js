e3e8628d04e602eafdfe3e306dea7bd6c6a4ef7b
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-22 22:35:35 -0700

    cleaned out commented out code/failures

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const getTripURL = (tripId) => {
 const loadTrips = () => {
   const tripsList = $('#trips-list');
   tripsList.empty();
-
-  // reportStatus('Loading trips...🛫');
-
   axios.get(URL)
     .then((response) => {
       console.log(response['data']);
@@ -24,22 +21,13 @@ const loadTrips = () => {
 };
 
 const getTrip = (tripID) => {
-  // console.log(getTripURL(tripID));
   const tripInfo = $('#trip-info');
   tripInfo.empty();
-
-  // tripURL =
-
   axios.get(getTripURL(tripID))
     .then((response) => {
       console.log(response);
-
       tripInfo.append(`<p>${response['data']['name']}</p><p>${response['data']['about']}</p><p>${response['data']['category']}</p>`)
 
-      // response['data'].forEach((trip) => {
-      //   // include message about successfully loading n number of trips
-      //   tripsList.append(`<li>${trip.name}</li>`)
-      // })
     })
     .catch((error) => {
       console.log(error)
@@ -50,17 +38,7 @@ const getTrip = (tripID) => {
 
 $(document).ready(() => {
   $('#load-trips-button').click(loadTrips);
-  // $('#trip-form').submit(createTrip);
   $('#trips-list').on('click', function(event) {
-    let foo = event.target.classList[1];
-    // console.log("hi");
-    // console.log(event.target.classList[1]);
-    // console.log(`"${$(this).html()}"`);
-    getTrip(foo);
-    // console.log(`results: "${$(this).html()}"`);
+    getTrip(event.target.classList[1]);
   });
 });
-
-// $(document).foundation();
-
-// .target.className
