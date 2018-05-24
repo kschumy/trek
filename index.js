525e296ea7af66b3d4ba649c9eab153c2f13c5a4
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-24 10:25:21 -0700

    styling and animation

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ const inputField = name => $(`#reservation-form input[name="${name}"]`);
 const getInput = name => { return inputField(name).val() || undefined };
 
 const reservationFormData = () => {
-    // const getInput = name => {
-    //   const input = inputField(name).val();
-    //   return input ? input : undefined;
-    // };
-    const formData = {};
-    FORM_FIELDS.forEach((field) => { formData[field] = getInput(field); });
-    return formData;
+  const formData = {};
+  FORM_FIELDS.forEach((field) => { formData[field] = getInput(field); });
+  return formData;
 };
 
 const clearForm = () => {
@@ -23,9 +19,7 @@ const URL = 'https://ada-backtrek-api.herokuapp.com/trips';
 const getTripURL = (tripId) => { return `${URL}/${tripId}` };
 const getReservationURL = (tripId) => { return `${URL}/${tripId}/reservations` };
 
-const reportStatus = (message) => {
-  $('#status-message').html(message);
-};
+const reportStatus = (message) => { $('#status-message').html(message); };
 
 const reportError = (message, errors) => {
   let content = '';
@@ -35,13 +29,12 @@ const reportError = (message, errors) => {
   reportStatus(`<p>${message}</p><ul>${content}</ul>`);
 };
 
-
 const loadTrips = () => {
   const tripsList = $('#trips-list');
   tripsList.empty();
   axios.get(URL)
     .then((response) => {
-      console.log(response['data']);
+      // console.log(response['data']);
       response['data'].forEach((trip) => {
         // include message about successfully loading n number of trips
         tripsList.append(`<li class="trip ${trip.id}">${trip.name}</li>`)
@@ -57,7 +50,7 @@ const getTrip = (tripID) => {
   tripInfo.empty();
   axios.get(getTripURL(tripID))
     .then((response) => {
-      console.log(response);
+      console.log(response['data']);
       tripInfo.append(`<h3 class='${tripID}'>${response['data']['name']}</h3><p>${response['data']['about']}</p><p>${response['data']['category']}</p>`);
       loadForm(`${response['data']['name']}`);
     })
@@ -100,8 +93,45 @@ const createReservation = (event) => {
 };
 
 $(document).ready(() => {
-  $('#load-trips-button').click(loadTrips);
-  $('#trips-list').on('click', function(event) { getTrip(event.target.classList[1]); });
+  $('.hidden-at-start').hide();
+  $('#load-trips-button').on('click', function(event) {
+    $('#list').slideDown('slow')
+    .promise().done(function() {
+
+      loadTrips(event);
+  });
+  });
+  // $('#trips-list').on('mousedown', function(event) {
+  //   $('.side-info').slideUp('slow');
+  // });
+
+  $('#trips-list').on('click', function(event) {
+    $('.side-info').slideUp('slow')
+      .promise().done(function() {
+      getTrip(event.target.classList[1]);
+      // $('.side-info').promise().done(function() {
+      //   $('.side-info').slideToggle('slow');
+      // });
+      $('.side-info').slideDown('slow');
+      // $( "p" ).append( " Finished! " );
+    });
+    // $('.side-info').slideUp('slow');
+    // $(this).is(':visible')? toggle_switch.text('Hide') : toggle_switch.text('Show');
+
+      // $('#book').slideToggle();
+      // getTrip(event.target.classList[1]);
+      // // $('.side-info').promise().done(function() {
+      // //   $('.side-info').slideToggle('slow');
+      // // });
+      // $('.side-info').slideDown('slow');
+
+  });
+
+
+
+  $('#trip-info').on('scroll', function() {
+    // $().css( "display", "inline" ).fadeOut( "slow" );
+  });
   $('#reservation-form').on('submit', function(event) { createReservation(event); });
 });
 
